6d191b584e242ca21e4c2baba3c62034f60600ca
NathStevo97 <natstephenson15@gmail.com>
2025-04-07 22:36:10 +0100

    chore: terraform and provider updates

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.9.2"
+  required_version = ">= 1.11.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.89.0"
+      version = ">= 5.94.0"
     }
     template = {
       source = "hashicorp/template"
