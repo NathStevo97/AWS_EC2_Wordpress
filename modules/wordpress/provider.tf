064257d2e79a06ec19e857466f79aa0dd2e266d5
NathStevo97 <natstephenson15@gmail.com>
2025-04-07 22:46:04 +0100

    chore: provider.tf for module

---
+++ modules/wordpress/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.11.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.94.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "Nathan-Dev"
+  region  = "eu-west-2"
+}
+
+provider "template" {
+  # Configuration options
+}
